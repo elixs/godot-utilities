b6120de0ca15d07a84159233b5a9259ed9270a9b
Elías Zelada <elixs.777@gmail.com>
2025-03-13 20:01:12 -0300

    new debug version

--- Debug.gd
+++ Debug.gd
@@ -4,23 +4,36 @@ extends Node
 @onready var canvas_layer  = CanvasLayer.new()
 @onready var container = VBoxContainer.new()
 
+var index = 0
+var window_title = ""
+
 
 func _ready() -> void:
-	if !OS.is_debug_build():
+	if not OS.is_debug_build():
 		return
 	add_child(canvas_layer)
-	canvas_layer.layer = 1000
+	canvas_layer.layer = 128
 	canvas_layer.add_child(container)
 	container.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	window_title = get_window().title
 
 
 func log(message: Variant, seconds: float = 2) -> void:
-	if multiplayer.multiplayer_peer is OfflineMultiplayerPeer:
-		print(message)
+	if not OS.is_debug_build():
+		return
+	if is_online():
+		var prefix = _get_prefix()
+		print_rich("[b]%s:[/b] " % prefix, message)
+		add_message.rpc("%s: %s" % [prefix, str(message)], seconds)
 	else:
-		print_rich("[b]%s:[/b] " % (("Server") if multiplayer.is_server() else "Client"), message)
+		add_message(str(message), seconds)
+		print(message)
+
+
+@rpc("any_peer", "reliable", "call_local")
+func add_message(message: String, seconds: float) -> void:
 	var label = Label.new()
-	label.text = str(message)
+	label.text = message
 	label.set("theme_override_constants/outline_size", 2)
 	label.set("theme_override_colors/font_outline_color", Color.BLACK)
 	container.add_child(label)
@@ -28,3 +41,28 @@ func log(message: Variant, seconds: float = 2) -> void:
 	await get_tree().create_timer(seconds).timeout
 	container.remove_child(label)
 	label.queue_free()
+
+
+func add_to_window_title(text: String) -> void:
+	if not OS.is_debug_build():
+		return
+	get_window().title = "%s - %s" % [window_title, text]
+
+
+func reset_window_title() -> void:
+	get_window().title = window_title
+
+
+func is_online() -> bool:
+	return multiplayer.multiplayer_peer and \
+		not multiplayer.multiplayer_peer is OfflineMultiplayerPeer and \
+		multiplayer.multiplayer_peer.get_connection_status() == MultiplayerPeer.CONNECTION_CONNECTED
+
+
+func _get_prefix() -> String:
+	if multiplayer.is_server():
+		return "Server"
+	elif index:
+		return "Client %d" % index
+	else:
+		return "Client"
